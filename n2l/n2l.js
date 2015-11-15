8c68bdd97d049f19164b2ae01a52d5c2c35316ed
Jordan Yoon-Buck <jyoon-buck17@students.browndeerschools.com>
2015-11-15 15:14:02 -0600

    rearranging, dom stuff, etc.

--- n2l/n2l.js
+++ n2l/n2l.js
@@ -1,41 +1,72 @@
+/* globals Util, Ph */
 var canvas = document.getElementById('c');
 var ctx = canvas.getContext('2d');
-var stage = {
+var n2l = {};
+n2l.stage = {
   width: 0, height: 0,
   mouse: {
     x: 0, y: 0
   }
 };
 
-canvas.style.cursor = 'none';
-
 // update the mouse x and y when the mouse moves
 window.addEventListener('mousemove', function(event) {
-  stage.mouse.x = event.clientX;
-  stage.mouse.y = event.clientY;
+  n2l.stage.mouse.x = event.clientX;
+  n2l.stage.mouse.y = event.clientY;
 });
 // update the stage and canvas size when the window's resized
 // IIFE magic to make this run once anyway
 window.addEventListener('resize', (function resizeListener() {
-  canvas.width = stage.width = window.innerWidth;
-  canvas.height = stage.height = window.innerHeight;
+  canvas.width = n2l.stage.width = window.innerWidth;
+  canvas.height = n2l.stage.height = window.innerHeight;
   return resizeListener;
 }()));
-var color = new util.Color();
-function draw() {
-  requestAnimationFrame(draw);
-  color.parse(
-    255 * stage.mouse.x / stage.width,
-    160,
-    255 * stage.mouse.y / stage.height);
-  ctx.fillStyle = "#" + color.hex();
-  ctx.strokeStyle = 'white';
-  ctx.fillRect(0, 0, stage.width, stage.height);
-  ctx.beginPath();
-  ctx.moveTo(0.5, stage.mouse.y + 0.5);
-  ctx.lineTo(stage.width + 0.5, stage.mouse.y + 0.5);
-  ctx.moveTo(stage.mouse.x + 0.5, 0.5);
-  ctx.lineTo(stage.mouse.x + 0.5, stage.height + 0.5);
-  ctx.stroke();
+
+
+// delete when used, for now just to make jshint shut up
+(function(){}(Util,Ph,ctx));
+
+n2l.scene = 0; // 0: falling
+               // 1: boat
+document.body.classList.add('scene-falling');
+
+function doTransition() {
+  document.body.classList.remove('scene-falling', 'scene-boat');
+  document.body.classList.add('scene-transitioning');
+  // do assorted transitioney stuff
+  document.body.classList.remove('scene-transitioning');
+  document.body.classList.add('scene-' + (n2l.scene ? 'boat' : 'falling'));
 }
-draw();
+
+(function setupSceneButton() {
+  n2l.$sceneToggle = document.getElementById('scene-toggle');
+  n2l.$sceneToggle.addEventListener('click', function() {
+    n2l.scene = 1^n2l.scene;
+    doTransition();
+  });
+}());
+
+(function setupDisplayPanel() {
+
+}());
+
+n2l.timing = {};
+n2l.timing.last = Date.now();
+n2l.timing.runningFPS = 60;
+function draw() {
+  // raf, timing
+  requestAnimationFrame(draw);
+  var thisTime = Date.now();
+  n2l.timing.delta = thisTime - n2l.timing.last;
+  n2l.timing.last = thisTime;
+  n2l.timing.fps = 1000 / n2l.timing.delta;
+  var runningFPS = (49 * n2l.timing.runningFPS + n2l.timing.fps) / 50;
+  if (runningFPS !== n2l.timing.runningFPS) {
+    n2l.timing.runningFPS = runningFPS;
+    document.getElementById('fps').innerText = Math.floor(runningFPS);
+  }
+  // main loop!
+
+
+}
+requestAnimationFrame(draw);
--- n2l/n2l.utils.js
+++ n2l/n2l.utils.js
@@ -1,38 +1,81 @@
-var util = {};
+var Util = {};
 
-util.Color = function() {
+Util.Color = function() {
   return this.parse.apply(this, arguments);
 };
-// usage: .parse("#RRGGBB")
-// usage: .parse("RRGGBB")
-// usage: .parse(0xRRGGBB)
-// usage: .parse("RR", "GG", "BB")
-// usage: .parse(0xRR, 0xGG, 0xBB)
-// usage: .parse(rrr, ggg, bbb)
-util.Color.prototype.parse = function(r, g, b) {
-  if (typeof r === 'string') {
-    // remove the "#" if there's one
-    if (r.slice(0, 1) === '#') r = r.slice(1);
-    // parse as hex
-    r = parseInt(r, 16);
-  }
-  if (typeof g === 'string') g = parseInt(g, 16);
-  if (typeof b === 'string') b = parseInt(b, 16);
-  if (r !== undefined && g === undefined && b === undefined) {
-    // most likely we got a color of the form 0xRRGGBB (or 'rrggbb')
-    var hex = r;
-    r = (hex >> 16);
-    g = (hex >> 8);
-    b = hex;
-  }
-  if (r !== undefined) {
-    this.r = r & 255;
-    this.g = g & 255;
-    this.b = b & 255;
-  }
-  return this;
+  // usage: .parse("#RRGGBB")
+  // usage: .parse("RRGGBB")
+  // usage: .parse(0xRRGGBB)
+  // usage: .parse("RR", "GG", "BB")
+  // usage: .parse(0xRR, 0xGG, 0xBB)
+  // usage: .parse(rrr, ggg, bbb)
+  Util.Color.prototype.parse = function(r, g, b) {
+    if (typeof r === 'string') {
+      // remove the "#" if there's one
+      if (r.slice(0, 1) === '#') r = r.slice(1);
+      // parse as hex
+      r = parseInt(r, 16);
+    }
+    if (typeof g === 'string') g = parseInt(g, 16);
+    if (typeof b === 'string') b = parseInt(b, 16);
+    if (r !== undefined && g === undefined && b === undefined) {
+      // most likely we got a color of the form 0xRRGGBB (or 'rrggbb')
+      var hex = r;
+      r = (hex >> 16);
+      g = (hex >> 8);
+      b = hex;
+    }
+    if (r !== undefined) {
+      this.r = r & 255;
+      this.g = g & 255;
+      this.b = b & 255;
+    }
+    return this;
+  };
+  Util.Color.prototype.hex = function() {
+    // http://stackoverflow.com/a/5624139
+    return ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1);
+  };
+
+
+Util.Color.lerp = function(color1, color2, level) {
+  level = (level === undefined ? 0.5 : Number(level));
+  return Util.Color.weightedAverage(
+    [color1, color2],
+    [1 - level, level]
+  );
 };
-util.Color.prototype.hex = function() {
-  // http://stackoverflow.com/a/5624139
-  return ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1);
+Util.Color.weightedAverage = function(colors, weights) {
+  var outputColor = new Util.Color();
+  ['r', 'g', 'b'].forEach(function(channel) {
+    var channelArray = [];
+    colors.forEach(function(color) {
+      channelArray.push(color[channel]);
+    });
+    var wAvgValue = Util.weightedAverage(channelArray, weights);
+    outputColor[channel] = Math.floor(wAvgValue);
+  });
+  return outputColor;
+};
+Util.lerp = function(number1, number2, level) {
+  level = (level === undefined ? 0.5 : Number(level));
+  return Util.weightedAverage(
+    [number1, number2],
+    [1 - level, level]
+  );
+};
+Util.sum = function(array) {
+  if (typeof array === 'number') {
+    return array;
+  }
+  array.push(0);
+  // now, the value on the right will always be a number.
+  return array.reduceRight(function(right, left) {
+    return right + Util.sum(left);
+  });
+};
+Util.weightedAverage = function(values, weights) {
+  return (Util.sum(values.map(function(number, index) {
+    return number * weights[index];
+  })) / Util.sum(weights));
 };
